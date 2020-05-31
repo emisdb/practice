16a2b650e18eed4c801d547e21c3150ecac09e9d
denis <emisdb@gmail.com>
2020-05-31 11:34:42 +0300

    initial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+'use strict';
+const http = require('http');
+const fs = require('fs');
+const html = fs.readFileSync('index.html', 'utf8');
+const server = http.createServer(function(request, response){
+   response.end(html);
+});
+server.listen('3000');
+
