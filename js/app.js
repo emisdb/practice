dcc0102bf50039222f1d24e39b8ee159ea173be7
denis <emisdb@gmail.com>
2020-06-10 03:43:02 +0300

    multiple experiments

--- js/app.js
+++ js/app.js
@@ -6,5 +6,27 @@ var params ={
             return this.size
         else
             return this.color;
+    },
+    test: function(obj){
+        test_func(obj)
     }
+}
+var callNumber=0;
+function go_inc(obj)
+{
+    setTimeout(go_inc, 3000, obj)
+    var intI = 0
+    intI++; callNumber++;
+    obj.innerHTML=":"+callNumber+":"+intI
+
+}
+function test_func(obj){
+    obj.innerHTML=this.toString('size')
+}
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    return xmlHttp.responseText;
 }
