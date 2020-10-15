050c209ec2354233907bf99b3222314137619c51
denis <emisdb@gmail.com>
2020-10-15 14:43:44 +0300

    modal box with list of values

--- js/index.js
+++ js/index.js
@@ -8,11 +8,35 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
+function emptyLi(ii){
+    var retval ="";
+    for (i=0;i<ii;i++){
+        retval += "<li>null</li>";
+    }
+    return retval;
+}
 btn.onclick = function() {
-    var mycontent = JSON.stringify(app.selection);
-//    var mycontent = app.selection.toString();
+    var prev =0;
+    var mycontent = '<ol start="0">' + app.selection.map((item,i) => {
+        var arr ="";
+        if(i>prev+1) arr+=emptyLi(i-prev-1);
+        arr +=  '<li>';
+        if(Array.isArray(item)) {
+            var pprev =0;
+            arr += '<ol  start="0">' + item.map((it,ii)=>{
+                 var arri = "";
+                 if(ii>pprev+1) arri+=emptyLi(ii-pprev-1);
+                 pprev = ii;
+                 arri += '<li>'+it+'</li>';
+                 return arri;}).join('') +'</ol>';
+         }else{ arr += item; }
+        arr += '</li>';
+        prev=i;
+        return arr;}).join('') + '</ol>';
     var myplace = document.getElementById("myModalContent");
     myplace.innerHTML =mycontent;
+    var myplace = document.getElementById("myModalFooter");
+    myplace.innerHTML =JSON.stringify(app.selection);
     modal.style.display = "block";
 }
 
